1713eea8802b8fe8ca567662656a80b2142c89f0
Momina1-stack <mominarare@gmail.com>
2025-09-16 12:10:43 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all js code as a newer version
+
+
+//alert(3+3) // we are using nodejs, not browser
+console.log(3
+    +3) // code readability should be high
+
+console.log("momina")
+let name="momina"
+let age=22
+let isLoggedIn=false
+let state;
+
+
+//number => 2 to power 53
+// bigint
+//string =>""
+// boolean ==> true/false
+//null => standalone value
+// undefined =>
+//symbol => unique
+
+//object
+
+console.log(typeof "momina")
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
