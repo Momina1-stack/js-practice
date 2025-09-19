0dcb372175b3d20d2f7f5975ef7e39cb7b8ea1d7
Momina1-stack <mominarare@gmail.com>
2025-09-19 16:25:26 +0000

    converstion

---
+++ 01_basics/03_converstionOperation.js
@@ -0,0 +1,22 @@
+let score = 33
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let stringToNumber=String (score);
+// console.log(typeof stringToNumber)
+let stringToNumber= Boolean (score);
+console.log(typeof stringToNumber)
+/*  "33" => 33
+"33abc" => NAN
+true => 1; false => 0
+
+ */
+
+let isloggedIn=1
+let booleanIsLoggedIn=Boolean(isloggedIn)
+console.log( booleanIsLoggedIn);
+
+// 1 => true; 0=>false
+// "" => false
+// "momina" => true
